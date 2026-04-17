e25b6ceba73712318c040f6da13b767aeaaec93d
Adam Noir <webnoircode@gmail.com>
2026-04-17 15:04:05 -0600

    first commit

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+import { defineCollection } from "astro:content";
+
+
+const blog = defineCollection({
+   loader: glob({pattern: '*.md', base: 'src/content/blog'}),
+    schema: () => z.object({
+        
+        title: z.string(),
+        author: z.string(),
+        desc: z.string(),
+        date: z.date(),
+        type: z.string()
+    })
+});
+
+export const collections = {
+    blog
+}
---
+++ src/content/blog/post-3.md
@@ -0,0 +1,15 @@
+---
+title: This is a post about the absurd of the life
+author: Ivan Gutierrez
+type: Teoria
+desc: Simple post hablando sobre notas de libros y otras pequeñas reflexiones sin importancia
+date: 2026-12-11
+---
+
+# Hola mundo
+
+contenido
+
+## Segundo titulo
+
+mas contenido
